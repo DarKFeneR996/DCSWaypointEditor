6bf0415527f4a6a4a746b7027a3979095bb90a9c
Viral-51stvfw <71588361+Viral-51stvfw@users.noreply.github.com>
2021-06-25 18:56:48 -0400

    Bug/fix elevation (#2)
    * fix elevation sign grouping
    
    * mo betta logging

--- src/gui.py
+++ src/gui.py
@@ -572,11 +572,12 @@ class GUI:
         self.profile = Profile('')
 
     def parse_map_coords_string(self, coords_string, tomcat_mode=False):
+        position = None
         coords_string = coords_string.upper()
         # "X-00199287 Z+00523070, 0 ft"   Not sure how to convert this yet
 
         # "37 T FJ 36255 11628, 5300 ft"  Tessaract did not like this one because the DCS font J looks too much like )
-        res = re.match("^(\d+ [a-zA-Z] [a-zA-Z][a-zA-Z] \d+ \d+), [-]?(\d+) (FT|M)", coords_string)
+        res = re.match("^(\d+ [a-zA-Z] [a-zA-Z][a-zA-Z] \d+ \d+), ([-]?\d+) (FT|M)", coords_string)
         if res is not None:
             mgrs_string = res.group(1).replace(" ", "")
             decoded_mgrs = mgrs.UTMtoLL(mgrs.decode(mgrs_string))
@@ -590,7 +591,7 @@ class GUI:
             return position, elevation
 
         # "N43°10.244 E40°40.204, 477 ft"  Degrees and decimal minutes
-        res = re.match("^([NS])(\d+)°([^\s]+) ([EW])(\d+)°([^,]+), [-]?(\d+) (FT|M)", coords_string)
+        res = re.match("^([NS])(\d+)°([^\s]+) ([EW])(\d+)°([^,]+), ([-]?\d+) (FT|M)", coords_string)
         if res is not None:
             lat_str = res.group(2) + " " + res.group(3) + " " + res.group(1)
             lon_str = res.group(5) + " " + res.group(6) + " " + res.group(4)
@@ -603,7 +604,7 @@ class GUI:
             return position, elevation
 
         # "N42-43-17.55 E40-38-21.69, 0 ft" Degrees, minutes and decimal seconds
-        res = re.match("^([NS])(\d+)-(\d+)-([^\s]+) ([EW])(\d+)-(\d+)-([^,]+), [-]?(\d+) (FT|M)", coords_string)
+        res = re.match("^([NS])(\d+)-(\d+)-([^\s]+) ([EW])(\d+)-(\d+)-([^,]+), ([-]?\d+) (FT|M)", coords_string)
         if res is not None:
             lat_str = res.group(2) + " " + res.group(3) + " " + res.group(4) + " " + res.group(1)
             lon_str = res.group(6) + " " + res.group(7) + " " + res.group(8) + " " + res.group(5)
@@ -616,7 +617,7 @@ class GUI:
             return position, elevation
 
         # "43°34'37"N 29°11'18"E, 0 ft" Degrees minutes and seconds
-        res = re.match("^(\d+)°(\d+)'([^\"]+)\"([NS]) (\d+)°(\d+)'([^\"]+)\"([EW]), [-]?(\d+) (FT|M)", coords_string)
+        res = re.match("^(\d+)°(\d+)'([^\"]+)\"([NS]) (\d+)°(\d+)'([^\"]+)\"([EW]), ([-]?\d+) (FT|M)", coords_string)
         if res is not None:
             lat_str = res.group(1) + " " + res.group(2) + " " + res.group(3) + " " + res.group(4)
             lon_str = res.group(5) + " " + res.group(6) + " " + res.group(7) + " " + res.group(8)
@@ -650,7 +651,11 @@ class GUI:
             elevation = self.capture_map_coords(2074, 97, 966, 32)
 
         self.captured_map_coords = str()
-        self.logger.info("Parsed captured text: " + str(position))
+
+        if position is not None:
+            self.logger.info("Parsed captured text: " + str(position))
+        else:
+            self.logger.info("Position was null for coords_string: " + str(coords_string))
         return position, elevation
 
     def input_parsed_coords(self):
