61074cd7c5d9493c9e548fd741bb32c4c4746daa
ilominar <85649993+ilominar@users.noreply.github.com>
2021-06-30 00:58:31 -0700

    Update prefs_manager.py
    Added default callsign parameter. Reduced DCS grace and button release delay defaults.

--- src/prefs_manager.py
+++ src/prefs_manager.py
@@ -2,26 +2,30 @@ from configparser import ConfigParser
 from pathlib import Path
 from src.gui_util import airframe_list, airframe_type_to_ui_text
 import os
+import re
 
 
 # preferences object to abstract preferences storage. preference values are always strings.
 #
 class PrefsManager:
     def __init__(self, file="settings.ini"):
-        self.prefs = ConfigParser()
         self.bs_file = file
+
+        self.prefs = ConfigParser()
+        self.prefs.add_section("PREFERENCES")
 
         self.path_dcs = f"{str(Path.home())}\\Saved Games\\DCS.openbeta\\"
         self.path_tesseract = f"{os.environ['PROGRAMW6432']}\\Tesseract-OCR\\tesseract.exe"
-        self.path_mission = f"{str(Path.home())}\\Desktop\\cf_mission.xml"
-        self.dcs_grace_period = "5"
-        self.dcs_btn_rel_delay_short = "0.2"
-        self.dcs_btn_rel_delay_medium = "0.5"
+        self.path_mission = f"{str(Path.home())}\\Desktop\\dcs_mission.xml"
+        self.dcs_grace_period = "4"
+        self.dcs_btn_rel_delay_short = "0.15"
+        self.dcs_btn_rel_delay_medium = "0.30"
         self.hotkey_capture = "ctrl+t"
         self.hotkey_capture_mode = "ctrl+shift+t"
         self.hotkey_enter_profile = "ctrl+alt+t"
-        self.hotkey_enter_mission = ""
+        self.hotkey_enter_mission = "ctrl+alt+shift+t"
         self.airframe_default = airframe_list()[0]
+        self.callsign_default = "Colt1-1"
         self.is_auto_upd_check = "true"
         self.is_tesseract_debug = "false"
         self.profile_db_name = "profiles.db"
@@ -31,8 +35,6 @@ class PrefsManager:
             self.synchronize_prefs()
             self.is_new_prefs_file = False
         except FileNotFoundError:
-            self.prefs.add_section("PREFERENCES")
-            self.reset_prefs()
             self.persist_prefs()
             self.is_new_prefs_file = True
 
@@ -163,6 +165,16 @@ class PrefsManager:
         self._airframe_default = value
 
     @property
+    def callsign_default(self):
+        return self._callsign_default
+    
+    @callsign_default.setter
+    def callsign_default(self, value):
+        if not self.is_callsign_valid(value):
+            raise ValueError("Invalid callsign")
+        self._callsign_default = value
+
+    @property
     def is_auto_upd_check(self):
         return self._is_auto_upd_check
 
@@ -204,20 +216,29 @@ class PrefsManager:
                 return False
         return True
 
+    # validate a callsign
+    #
+    def is_callsign_valid(self, callsign):
+        if not re.match(r"^[\D]+[\d]+-[\d]+$", callsign):
+            return False
+        else:
+            return True
+
     # reset the preferences to their default values, must persist via persist_prefs to save.
     #
     def reset_prefs(self):
         self.path_dcs = f"{str(Path.home())}\\Saved Games\\DCS.openbeta\\"
         self.path_tesseract = f"{os.environ['PROGRAMW6432']}\\Tesseract-OCR\\tesseract.exe"
         self.path_mission = f"{str(Path.home())}\\Desktop\\cf_mission.xml"
-        self.dcs_grace_period = "5"
-        self.dcs_btn_rel_delay_short = "0.2"
-        self.dcs_btn_rel_delay_medium = "0.5"
+        self.dcs_grace_period = "4"
+        self.dcs_btn_rel_delay_short = "0.15"
+        self.dcs_btn_rel_delay_medium = "0.30"
         self.hotkey_capture = "ctrl+t"
         self.hotkey_capture_mode = "ctrl+shift+t"
         self.hotkey_enter_profile = "ctrl+alt+t"
         self.hotkey_enter_mission = "ctrl+alt+shift+t"
         self.airframe_default = airframe_list()[0]
+        self.callsign_default = "Colt1-1"
         self.is_auto_upd_check = "true"
         self.is_tesseract_debug = "false"
         self.profile_db_name = "profiles.db"
@@ -225,6 +246,7 @@ class PrefsManager:
     # synchronize the preferences the backing store file
     #
     def synchronize_prefs(self):
+        self.persist_prefs(do_write=False)
         self.prefs.read(self.bs_file)
 
         self.path_dcs = self.prefs["PREFERENCES"]["path_dcs"]
@@ -238,13 +260,14 @@ class PrefsManager:
         self.hotkey_enter_profile = self.prefs["PREFERENCES"]["hotkey_enter_profile"]
         self.hotkey_enter_mission = self.prefs["PREFERENCES"]["hotkey_enter_mission"]
         self.airframe_default = self.prefs["PREFERENCES"]["airframe_default"]
+        self.callsign_default = self.prefs["PREFERENCES"]["callsign_default"]
         self.is_auto_upd_check = self.prefs["PREFERENCES"]["is_auto_upd_check"]
         self.is_tesseract_debug = self.prefs["PREFERENCES"]["is_tesseract_debug"]
         self.profile_db_name = self.prefs["PREFERENCES"]["profile_db_name"]
 
     # persist the preferences to the backing store file
     #
-    def persist_prefs(self):
+    def persist_prefs(self, do_write=True):
         self.prefs["PREFERENCES"]["path_dcs"] = self.path_dcs
         self.prefs["PREFERENCES"]["path_tesseract"] = self.path_tesseract
         self.prefs["PREFERENCES"]["path_mission"] = self.path_mission
@@ -256,9 +279,11 @@ class PrefsManager:
         self.prefs["PREFERENCES"]["hotkey_enter_profile"] = self.hotkey_enter_profile
         self.prefs["PREFERENCES"]["hotkey_enter_mission"] = self.hotkey_enter_mission
         self.prefs["PREFERENCES"]["airframe_default"] = self.airframe_default
+        self.prefs["PREFERENCES"]["callsign_default"] = self.callsign_default
         self.prefs["PREFERENCES"]["is_auto_upd_check"] = self.is_auto_upd_check
         self.prefs["PREFERENCES"]["is_tesseract_debug"] = self.is_tesseract_debug
         self.prefs["PREFERENCES"]["profile_db_name"] = self.profile_db_name
 
-        with open(self.bs_file, "w+") as f:
-            self.prefs.write(f)
+        if do_write:
+            with open(self.bs_file, "w+") as f:
+                self.prefs.write(f)
