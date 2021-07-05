63ce4cc317ea84247be8b13f55b3bb93fa454e7d
ilominar <85649993+ilominar@users.noreply.github.com>
2021-07-05 14:55:44 -0700

    Update prefs_gui.py
    Better handling of no DCS-BIOS. Now allow prefs to be closed, but warn functionality might be degraded.

--- src/prefs_gui.py
+++ src/prefs_gui.py
@@ -75,69 +75,68 @@ class DCSWEPreferencesGUI:
         dcs_bios_ver = dcs_bios_vers_install(self.prefs.path_dcs)
 
         layout_paths = [
-            [PyGUI.Text("DCS user directory:", (20,1), justification="right"),
-            PyGUI.Input(self.prefs.path_dcs, key='ux_path_dcs', enable_events=True),
-            PyGUI.Button("Browse...", button_type=PyGUI.BUTTON_TYPE_BROWSE_FOLDER, target='ux_path_dcs')],
+            [PyGUI.Text("DCS saved games directory:", (21,1), justification="right"),
+             PyGUI.Input(self.prefs.path_dcs, key='ux_path_dcs', enable_events=True),
+             PyGUI.Button("Browse...", button_type=PyGUI.BUTTON_TYPE_BROWSE_FOLDER, target='ux_path_dcs')],
 
-            [PyGUI.Text("Tesseract executable:", (20,1), justification="right"),
-            PyGUI.Input(self.prefs.path_tesseract, key='ux_path_tesseract'),
-            PyGUI.Button("Browse...", button_type=PyGUI.BUTTON_TYPE_BROWSE_FILE, target='ux_path_tesseract')],
-
-            [PyGUI.Text("Mission file:", (20,1), justification="right"),
-            PyGUI.Input(self.prefs.path_mission, key='ux_path_mission'),
-            PyGUI.Button("Browse...", button_type=PyGUI.BUTTON_TYPE_BROWSE_FILE, target='ux_path_mission')],
+            [PyGUI.Text("Tesseract executable:", (21,1), justification="right"),
+             PyGUI.Input(self.prefs.path_tesseract, key='ux_path_tesseract'),
+             PyGUI.Button("Browse...", button_type=PyGUI.BUTTON_TYPE_BROWSE_FILE, target='ux_path_tesseract')],
+ 
+            [PyGUI.Text("Mission file:", (21,1), justification="right"),
+             PyGUI.Input(self.prefs.path_mission, key='ux_path_mission'),
+             PyGUI.Button("Browse...", button_type=PyGUI.BUTTON_TYPE_BROWSE_FILE, target='ux_path_mission')],
         ]
         layout_hotkeys = [
-            [PyGUI.Text("DCS F10 map capture:", (20,1), justification="right"),
-            PyGUI.Input(self.prefs.hotkey_capture, key='ux_hotkey_capture', pad=((5,80),0))],
+            [PyGUI.Text("DCS F10 map capture:", (21,1), justification="right"),
+             PyGUI.Input(self.prefs.hotkey_capture, key='ux_hotkey_capture', pad=((5,80),0))],
 
-            [PyGUI.Text("Toggle capture mode:", (20,1), justification="right"),
-            PyGUI.Input(self.prefs.hotkey_capture_mode, key='ux_hotkey_capture_mode')],
+            [PyGUI.Text("Toggle capture mode:", (21,1), justification="right"),
+             PyGUI.Input(self.prefs.hotkey_capture_mode, key='ux_hotkey_capture_mode')],
 
-            [PyGUI.Text("Load current profile into jet:", (20,1), justification="right"),
-            PyGUI.Input(self.prefs.hotkey_enter_profile, key='ux_hotkey_enter_profile')],
+            [PyGUI.Text("Load current profile into jet:", (21,1), justification="right"),
+             PyGUI.Input(self.prefs.hotkey_enter_profile, key='ux_hotkey_enter_profile')],
 
-            [PyGUI.Text("Load mission file into jet:", (20,1), justification="right"),
-            PyGUI.Input(self.prefs.hotkey_enter_mission, key='ux_hotkey_enter_mission')]
+            [PyGUI.Text("Load mission file into jet:", (21,1), justification="right"),
+             PyGUI.Input(self.prefs.hotkey_enter_mission, key='ux_hotkey_enter_mission')]
         ]
         layout_dcsbios = [
-            [PyGUI.Text("Grace period:", (20,1), justification="right"),
-            PyGUI.Input(self.prefs.dcs_grace_period, key='ux_dcs_grace_period'),
-            PyGUI.Text("(seconds)", justification="left", pad=((0,14),0))],
+            [PyGUI.Text("Grace period:", (21,1), justification="right"),
+             PyGUI.Input(self.prefs.dcs_grace_period, key='ux_dcs_grace_period'),
+             PyGUI.Text("(seconds)", justification="left", pad=((0,14),0))],
 
-            [PyGUI.Text("Button release (short):", (20,1), justification="right"),
-            PyGUI.Input(self.prefs.dcs_btn_rel_delay_short, key='ux_dcs_btn_rel_delay_short'),
-            PyGUI.Text("(seconds)", justification="left", pad=((0,14),0))],
+            [PyGUI.Text("Button release (short):", (21,1), justification="right"),
+             PyGUI.Input(self.prefs.dcs_btn_rel_delay_short, key='ux_dcs_btn_rel_delay_short'),
+             PyGUI.Text("(seconds)", justification="left", pad=((0,14),0))],
 
-            [PyGUI.Text("Button release (medium):", (20,1), justification="right"),
-            PyGUI.Input(self.prefs.dcs_btn_rel_delay_medium, key='ux_dcs_btn_rel_delay_medium'),
-            PyGUI.Text("(seconds)", justification="left", pad=((0,14),0))],
+            [PyGUI.Text("Button release (medium):", (21,1), justification="right"),
+             PyGUI.Input(self.prefs.dcs_btn_rel_delay_medium, key='ux_dcs_btn_rel_delay_medium'),
+             PyGUI.Text("(seconds)", justification="left", pad=((0,14),0))],
 
-            [PyGUI.Text("DCS-BIOS:", (20,1), justification="right"),
-            PyGUI.Text("Status", key='ux_dcs_bios_stat', size=(19,1)),
-            PyGUI.Button("Install", key='ux_install', size=(18,1),
-                         disabled=(dcs_bios_ver is not None))]
+            [PyGUI.Text("DCS-BIOS:", (21,1), justification="right"),
+             PyGUI.Text("Status", key='ux_dcs_bios_stat', size=(19,1)),
+             PyGUI.Button("Install", key='ux_install', size=(18,1),
+                          disabled=(dcs_bios_ver is not None))]
         ]
         layout_misc = [
-            [PyGUI.Text("Default airframe:", (20,1), justification="right"),
-            PyGUI.Combo(values=airframe_list(),
-                        default_value=airframe_type_to_ui_text(self.prefs.airframe_default),
-                        key='ux_airframe_default', pad=((5,277),0))],
+            [PyGUI.Text("Default airframe:", (21,1), justification="right"),
+             PyGUI.Combo(values=airframe_list(),
+                         default_value=airframe_type_to_ui_text(self.prefs.airframe_default),
+                         key='ux_airframe_default', pad=((5,277),0))],
 
-            [PyGUI.Text("Check for updates:", (20,1), justification="right"),
-            PyGUI.Checkbox(text="", default=is_auto_upd_check, key='ux_is_auto_upd_check')],
+            [PyGUI.Text("Check for updates:", (21,1), justification="right"),
+             PyGUI.Checkbox(text="", default=is_auto_upd_check, key='ux_is_auto_upd_check')],
 
-            [PyGUI.Text("Log raw OCR output:", (20,1), justification="right"),
-            PyGUI.Checkbox(text="", default=is_tesseract_debug, key='ux_is_tesseract_debug')]
+            [PyGUI.Text("Log raw OCR output:", (21,1), justification="right"),
+             PyGUI.Checkbox(text="", default=is_tesseract_debug, key='ux_is_tesseract_debug')]
         ]
 
         return PyGUI.Window("Preferences",
                             [[PyGUI.Frame("Paths & Files", layout_paths)],
-                            [PyGUI.Frame("DCS Hot Keys", layout_hotkeys)],
-                            [PyGUI.Frame("DCS BIOS Parameters", layout_dcsbios)],
-                            [PyGUI.Frame("Miscellaneous", layout_misc)],
-                            [PyGUI.Button("OK", key='ux_ok', size=(8,1), pad=((254,0),16),
-                                          disabled=(dcs_bios_ver is None))]],
+                             [PyGUI.Frame("DCS Hot Keys", layout_hotkeys)],
+                             [PyGUI.Frame("DCS BIOS Parameters", layout_dcsbios)],
+                             [PyGUI.Frame("Miscellaneous", layout_misc)],
+                             [PyGUI.Button("OK", key='ux_ok', size=(8,1), pad=((264,0),16))]],
                             modal=True, disable_close=True, finalize=True)
 
     # update gui for changes to the dcs path
@@ -146,13 +145,9 @@ class DCSWEPreferencesGUI:
         db_ver = dcs_bios_vers_install(self.prefs.path_dcs)
         if db_ver is None:
             db_ver_latest = dcs_bios_vers_latest()
-            self.window['ux_ok'].update(disabled=True)
-            self.window['ux_ok'].metadata = "true"
             self.window['ux_dcs_bios_stat'].update(value="Not Detected")
             self.window['ux_install'].update(text=f"Install v{db_ver_latest}", disabled=False)
         else:
-            self.window['ux_ok'].update(disabled=False)
-            self.window['ux_ok'].metadata = "false"
             self.window['ux_dcs_bios_stat'].update(value=f"Version {db_ver} Installed")
             if dcs_bios_is_current(self.prefs.path_dcs):
                 self.window['ux_install'].update(text=f"Install v{dcs_bios_vers_latest()}",
@@ -173,24 +168,26 @@ class DCSWEPreferencesGUI:
             self.logger.debug(f"Prefs Event: {event}")
             self.logger.debug(f"Prefs Values: {self.values}")
 
-            if event is None:
-                if (self.window['ux_ok'].metadata == "true"):
-                    PyGUI.Popup("Invalid preference value(s), changes will be ignored.", title="Error")
-                is_accepted = False
-                break
-
-            dcs_path = self.values['ux_path_dcs']
-            if dcs_path is not None and not dcs_path.endswith("\\") and not dcs_path.endswith("/"):
-                dcs_path = dcs_path + "\\"
+            path_dcs = self.values['ux_path_dcs']
+            if path_dcs is not None and not path_dcs.endswith("\\") and not path_dcs.endswith("/"):
+                path_dcs = path_dcs + "\\"
 
             if event == 'ux_ok':
-                self.prefs_persist(self.values)
-                break
+                if not os.path.exists(self.values['ux_path_dcs']):
+                    PyGUI.Popup("A valid path to the saved games tree for your DCS installation is required.",
+                                title="Error")
+                    continue
+                else:
+                    if not dcs_bios_vers_install(path_dcs):
+                        PyGUI.Popup("DCS-BIOS not detected. Some functionality will not be available.",
+                                    title="Note")
+                    self.prefs_persist(self.values)
+                    break
 
             elif event == 'ux_install':
                 try:
                     self.logger.info("Installing DCS BIOS...")
-                    dcs_bios_install(dcs_path)
+                    dcs_bios_install(path_dcs)
                 except (FileExistsError, FileNotFoundError, requests.HTTPError) as e:
                     self.logger.error("DCS-BIOS failed to install", exc_info=True)
                     PyGUI.Popup(f"DCS-BIOS failed to install:\n{e}", title="Error")
@@ -198,9 +195,6 @@ class DCSWEPreferencesGUI:
     
             elif event == 'ux_path_dcs':
                 self.update_gui_for_dcs_path()
-                if not os.path.exists(dcs_path):
-                    PyGUI.Popup("Invalid DCS path, unable to install DCS-BIOS.", title="Error")
-                    self.window['ux_install'].update(disabled=True)
 
         self.close()
         
