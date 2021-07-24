5359d84bb7f39272d3f45455573f56d99222f128
ilominar <85649993+ilominar@users.noreply.github.com>
2021-07-24 16:13:45 -0700

    Update prefs_gui.py
    Changed preferences GUI to now enable the window close widget. This is the lesser of multiple UX evils. Significant changes to field validation.

--- src/prefs_gui.py
+++ src/prefs_gui.py
@@ -21,6 +21,7 @@
 
 import os
 import PySimpleGUI as PyGUI
+from PySimpleGUI.PySimpleGUI import PopupOKCancel, T
 import requests
 
 from src.comp_dcs_bios import dcs_bios_vers_install, dcs_bios_vers_latest, dcs_bios_is_current
@@ -38,62 +39,88 @@ class PreferencesGUI:
         self.values = None
         self.window = self.create_gui()
 
-    # persist preferences in settings.ini based on values extracted from the prefs window.
+    # persist valid preferences to settings.ini based on values extracted from the prefs window.
+    # returns string CSV list of prefs with errors, None if no errors.
     #
     def prefs_persist(self, values):
         self.logger.info("Persisting preferences...")
-        self.prefs.path_dcs = values.get('ux_path_dcs')
-        self.prefs.path_tesseract = values.get('ux_path_tesseract')
-        self.prefs.path_mission = values.get('ux_path_mission')
         errors = ""
+
+        path_dcs = values.get('ux_path_dcs')
+        if path_dcs is not None and not path_dcs.endswith("\\") and not path_dcs.endswith("/"):
+            path_dcs = path_dcs + "\\"
+        if path_dcs is not None and os.path.exists(path_dcs):
+            self.prefs.path_dcs = values.get('ux_path_dcs')
+        else:
+            errors = errors + "DCS path, "
+
+        path_tesseract = values.get('ux_path_tesseract')
+        if path_tesseract is not None and os.path.exists(path_tesseract):
+            self.prefs.path_tesseract = values.get('ux_path_tesseract')
+        else:
+            errors = errors + "Tesseract path, "
+
+        self.prefs.path_mission = values.get('ux_path_mission')
+
         try:
             self.prefs.airframe_default = airframe_ui_text_to_type(values.get('ux_airframe_default'))
         except:
-            errors = "default airframe"
+            errors = errors + "default airframe, "
+
         try:
             self.prefs.av_setup_default = values.get('ux_av_setup_default')
             if self.prefs.av_setup_default not in AvionicsSetupModel.list_all_names():
                 raise Exception("Unknown avionics setup")
         except:
-            self.prefs.av_setup_default = "DCS Default"
+            self.prefs.av_setup_default = "DCS Default, "
+
         try:
             self.prefs.dcs_btn_rel_delay_short = values.get('ux_dcs_btn_rel_delay_short')
         except:
-            errors = errors + ", short release"
+            errors = errors + "short release, "
+
         try:
             self.prefs.dcs_btn_rel_delay_medium = values.get('ux_dcs_btn_rel_delay_medium')
         except:
-            errors = errors + ", medium release"
+            errors = errors + "medium release, "
+
         try:
             self.prefs.hotkey_capture = values.get('ux_hotkey_capture')
         except:
-            errors = errors + ", capture hotkey"
+            errors = errors + "'F10 Capture' hotkey, "
+
         try:
             self.prefs.hotkey_capture_mode = values.get('ux_hotkey_capture_mode')
         except:
-            errors = errors + ", capture quick hotkey"
+            errors = errors + "'F10 Capture Mode' hotkey, "
+
         try:
             self.prefs.hotkey_enter_profile = values.get('ux_hotkey_enter_profile')
         except:
-            errors = errors + ", enter profile hotkey"
+            errors = errors + "'Load Profile' hotkey, "
+
         try:
             self.prefs.hotkey_enter_mission = values.get('ux_hotkey_enter_mission')
         except:
-            errors = errors + ", enter mission hotkey"
+            errors = errors + "'Load Mission' hotkey, "
+
         try:
             self.prefs.hotkey_dgft_dogfight = values.get('ux_hotkey_dgft_dogfight')
         except:
-            errors = errors + ", DGFT dogfight hotkey"
+            errors = errors + "'DGFT Dogfight' hotkey, "
+
         try:
             self.prefs.hotkey_dgft_center = values.get('ux_hotkey_dgft_center')
         except:
-            errors = errors + ", DGFT center hotkey"
-        if len(errors) > 0:
-            PyGUI.Popup(f"Invalid value(s) for {errors}, changes ignored.", title="Error")
+            errors = errors + "'DGFT Center' hotkey, "
+
         self.prefs.is_auto_upd_check = values.get('ux_is_auto_upd_check')
         self.prefs.is_tesseract_debug = values.get('ux_is_tesseract_debug')
         self.prefs.is_av_setup_for_unk = values.get('ux_av_setup_unknown')
+
         self.prefs.persist_prefs()
+
+        return errors[:-2] if len(errors) > 0 else None
 
     # build the ui for the preferences window.
     #
@@ -114,46 +141,57 @@ class PreferencesGUI:
         layout_paths = [
             [PyGUI.Text("DCS saved games directory:", (21,1), justification="right"),
              PyGUI.Input(self.prefs.path_dcs, key='ux_path_dcs', enable_events=True),
-             PyGUI.Button("Browse...", button_type=PyGUI.BUTTON_TYPE_BROWSE_FOLDER, target='ux_path_dcs')],
+             PyGUI.Button("Browse...", button_type=PyGUI.BUTTON_TYPE_BROWSE_FOLDER,
+                          target='ux_path_dcs')],
 
             [PyGUI.Text("Tesseract executable:", (21,1), justification="right"),
-             PyGUI.Input(self.prefs.path_tesseract, key='ux_path_tesseract'),
-             PyGUI.Button("Browse...", button_type=PyGUI.BUTTON_TYPE_BROWSE_FILE, target='ux_path_tesseract')],
+             PyGUI.Input(self.prefs.path_tesseract, key='ux_path_tesseract', enable_events=True),
+             PyGUI.Button("Browse...", button_type=PyGUI.BUTTON_TYPE_BROWSE_FILE,
+                          target='ux_path_tesseract')],
  
             [PyGUI.Text("Mission file:", (21,1), justification="right"),
-             PyGUI.Input(self.prefs.path_mission, key='ux_path_mission'),
-             PyGUI.Button("Browse...", button_type=PyGUI.BUTTON_TYPE_BROWSE_FILE, target='ux_path_mission')],
+             PyGUI.Input(self.prefs.path_mission, key='ux_path_mission', enable_events=True),
+             PyGUI.Button("Browse...", button_type=PyGUI.BUTTON_TYPE_BROWSE_FILE,
+                          target='ux_path_mission')],
         ]
 
         layout_hotkeys = [
             [PyGUI.Text("DCS F10 map capture:", (21,1), justification="right"),
-             PyGUI.Input(self.prefs.hotkey_capture, key='ux_hotkey_capture', pad=((5,80),0))],
+             PyGUI.Input(self.prefs.hotkey_capture, key='ux_hotkey_capture', enable_events=True,
+                         pad=((5,80),0))],
 
             [PyGUI.Text("Toggle capture mode:", (21,1), justification="right"),
-             PyGUI.Input(self.prefs.hotkey_capture_mode, key='ux_hotkey_capture_mode')],
+             PyGUI.Input(self.prefs.hotkey_capture_mode, key='ux_hotkey_capture_mode',
+                         enable_events=True)],
 
             [PyGUI.Text("Load current profile into jet:", (21,1), justification="right"),
-             PyGUI.Input(self.prefs.hotkey_enter_profile, key='ux_hotkey_enter_profile')],
+             PyGUI.Input(self.prefs.hotkey_enter_profile, key='ux_hotkey_enter_profile',
+                         enable_events=True)],
 
             [PyGUI.Text("Load mission file into jet:", (21,1), justification="right"),
-             PyGUI.Input(self.prefs.hotkey_enter_mission, key='ux_hotkey_enter_mission')],
+             PyGUI.Input(self.prefs.hotkey_enter_mission, key='ux_hotkey_enter_mission',
+                         enable_events=True)],
 
             [PyGUI.Text("", font="Helvetica 6", pad=(0,0))],
 
             [PyGUI.Text("F-16 HOTAS DGFT dogfight:", (21,1), justification="right"),
-             PyGUI.Input(self.prefs.hotkey_dgft_dogfight, key='ux_hotkey_dgft_dogfight')],
+             PyGUI.Input(self.prefs.hotkey_dgft_dogfight, key='ux_hotkey_dgft_dogfight',
+                         enable_events=True)],
 
             [PyGUI.Text("F-16 HOTAS DGFT center:", (21,1), justification="right"),
-             PyGUI.Input(self.prefs.hotkey_dgft_center, key='ux_hotkey_dgft_center')]
+             PyGUI.Input(self.prefs.hotkey_dgft_center, key='ux_hotkey_dgft_center',
+                         enable_events=True)]
         ]
 
         layout_dcsbios = [
             [PyGUI.Text("Button press (short):", (21,1), justification="right"),
-             PyGUI.Input(self.prefs.dcs_btn_rel_delay_short, key='ux_dcs_btn_rel_delay_short'),
+             PyGUI.Input(self.prefs.dcs_btn_rel_delay_short, key='ux_dcs_btn_rel_delay_short',
+                         enable_events=True),
              PyGUI.Text("(seconds)", justification="left", pad=((0,14),0))],
 
             [PyGUI.Text("Button press (medium):", (21,1), justification="right"),
-             PyGUI.Input(self.prefs.dcs_btn_rel_delay_medium, key='ux_dcs_btn_rel_delay_medium'),
+             PyGUI.Input(self.prefs.dcs_btn_rel_delay_medium, key='ux_dcs_btn_rel_delay_medium',
+                         enable_events=True),
              PyGUI.Text("(seconds)", justification="left", pad=((0,14),0))],
 
             [PyGUI.Text("DCS-BIOS:", (21,1), justification="right"),
@@ -169,7 +207,8 @@ class PreferencesGUI:
                          key='ux_airframe_default', readonly=True, enable_events=True,
                          size=(27,1), pad=((5,194),0))],
 
-            [PyGUI.Text("Default avionics setup:", (21,1), key='ux_av_setup_txt', justification="right"),
+            [PyGUI.Text("Default avionics setup:", (21,1), key='ux_av_setup_txt',
+                        justification="right"),
              PyGUI.Combo(values=as_tmplts, default_value=self.prefs.av_setup_default,
                          key='ux_av_setup_default', readonly=True, enable_events=True,
                          size=(27,1), pad=((6,2),8)),
@@ -189,7 +228,7 @@ class PreferencesGUI:
                              [PyGUI.Frame("DCS BIOS Parameters", layout_dcsbios)],
                              [PyGUI.Frame("Miscellaneous", layout_misc)],
                              [PyGUI.Button("OK", key='ux_ok', size=(8,1), pad=((264,0),16))]],
-                            modal=True, disable_close=True, finalize=True)
+                            modal=True, finalize=True)
 
     # update gui for changes to the dcs path
     #
@@ -224,6 +263,65 @@ class PreferencesGUI:
             self.window['ux_av_setup_txt'].update(text_color="#b8b8b8")
             self.window['ux_av_setup_unknown'].update(visible=False)
 
+    def validate_none(self, value, quiet=False):
+        return
+
+    def validate_dcs_path(self, value, quiet=False):
+        if os.path.exists(value):
+            self.prefs_persist(self.values)
+            self.update_gui_for_dcs_path()
+        elif not quiet:
+            PyGUI.Popup("A valid path to the saved games tree for your DCS installation is" +
+                        " required. Some functionality may be unavailable if the path is invalid.",
+                        title="Invalid DCS Path")
+
+    def validate_tesseract_path(self, value, quiet=False):
+        if os.path.exists(value):
+            self.prefs_persist(self.values)
+        elif not quiet:
+            PyGUI.Popup("A valid path to Tesseract is required. Some functionality may be" +
+                        " unavailable if the path is invalid.",
+                        title="Invalid Tesseract Path")
+    
+    def core_validate_hot_key(self, value, key, quiet=False):
+        if self.prefs.is_hotkey_valid(value):
+            self.prefs_persist(self.values)
+        elif not quiet:
+            PyGUI.Popup(f"The '{key}' hotkey specification is invalid.", title="Invalid Hotkey")
+
+    def validate_capture_hot_key(self, value, quiet=False):
+        self.core_validate_hot_key(value, "F10 Capture", quiet)
+
+    def validate_capture_mode_hot_key(self, value, quiet=False):
+        self.core_validate_hot_key(value, "F10 Capture Mode", quiet)
+
+    def validate_enter_profile_hot_key(self, value, quiet=False):
+        self.core_validate_hot_key(value, "Load Current Profile", quiet)
+
+    def validate_enter_mission_hot_key(self, value, quiet=False):
+        self.core_validate_hot_key(value, "Load Mission", quiet)
+
+    def validate_dog_dog_hot_key(self, value, quiet=False):
+        self.core_validate_hot_key(value, "DGFT Dogfight", quiet)
+
+    def validate_dog_center_hot_key(self, value, quiet=False):
+        self.core_validate_hot_key(value, "DGFT Center", quiet)
+
+    def core_validate_duration(self, value, type, quiet=False):
+        try:
+            if float(value) <= 0.0:
+                raise Exception("Invalid value")
+            self.prefs_persist(self.values)
+        except:
+            if not quiet:
+                PyGUI.Popup(f"The {type} button press duration is invalid.", title="Invalid Duration")
+
+    def validate_rds_duration(self, value, quiet=False):
+        self.core_validate_duration(value, "short", quiet)
+
+    def validate_rdm_duration(self, value, quiet=False):
+        self.core_validate_duration(value, "medium", quiet)
+
     # run the gui for the preferences window.
     #
     def run(self):
@@ -234,37 +332,69 @@ class PreferencesGUI:
         self.update_gui_for_dcs_path()
         self.update_gui_for_airframe_avsetup()
 
+        edit_text_val_map = { 'ux_path_dcs' : self.validate_dcs_path,
+                              'ux_path_tesseract' : self.validate_tesseract_path,
+                              'ux_path_mission' : self.validate_none,
+                              'ux_hotkey_capture' : self.validate_capture_hot_key,
+                              'ux_hotkey_capture_mode' : self.validate_capture_mode_hot_key,
+                              'ux_hotkey_enter_profile' : self.validate_enter_profile_hot_key,
+                              'ux_hotkey_enter_mission' : self.validate_enter_mission_hot_key,
+                              'ux_hotkey_dgft_dogfight' : self.validate_dog_dog_hot_key,
+                              'ux_hotkey_dgft_center' : self.validate_dog_center_hot_key,
+                              'ux_dcs_btn_rel_delay_short' : self.validate_rds_duration,
+                              'ux_dcs_btn_rel_delay_medium' : self.validate_rdm_duration
+        }
+
         while True:
-            event, self.values = self.window.Read()
+            new_event, new_values = self.window.Read()
             self.logger.debug(f"Prefs Event: {event}")
             self.logger.debug(f"Prefs Values: {self.values}")
+            if new_values is not None:
+                self.values = new_values
 
-            path_dcs = self.values['ux_path_dcs']
-            if path_dcs is not None and not path_dcs.endswith("\\") and not path_dcs.endswith("/"):
-                path_dcs = path_dcs + "\\"
-
-            if event == 'ux_ok':
-                if not os.path.exists(self.values['ux_path_dcs']):
-                    PyGUI.Popup("A valid path to the saved games tree for your DCS installation is required.",
-                                title="Error")
-                    continue
+            self.logger.debug(f"{event} / {new_event}")
+            if event != new_event and event in edit_text_val_map.keys():
+                if event == 'ux_ok' or event is None:
+                    (edit_text_val_map[event])(self.values[event], quiet=True)
                 else:
-                    if not dcs_bios_vers_install(path_dcs):
-                        PyGUI.Popup("DCS-BIOS not detected. Some functionality will not be available.",
-                                    title="Note")
-                    self.prefs_persist(self.values)
-                    break
+                    (edit_text_val_map[event])(self.values[event], quiet=False)
+            event = new_event
+
+            if event == 'ux_ok' or event is None:
+                errors = self.prefs_persist(self.values)
+                if errors is not None:
+                    if errors.count(',') >= 2:
+                        msg_inval = f"Invalid values for {errors}"
+                        msg_action = f"Changes to these preferences will be ignored."
+                        title = "Invalid Preferences"
+                    else:
+                        msg_inval = f"Invalid value for {errors}"
+                        msg_action = f"Changes to this preference will be ignored."
+                        title = "Invalid Preference"
+                    if event == 'ux_ok' and \
+                       PyGUI.PopupOKCancel(f"{msg_inval}. Are you sure you want to close the window?",
+                                           title=title) == "Cancel":
+                        continue
+                    elif event != 'ux_ok':
+                        PyGUI.Popup(f"{msg_inval}. {msg_action}", title=title)
+                if not dcs_bios_vers_install(self.prefs.path_dcs):
+                    PyGUI.Popup("DCS-BIOS not detected. Some functionality may not be available.",
+                                title="Note")
+                break
 
             elif event == 'ux_install':
+                self.prefs_persist(self.values)
                 try:
                     self.logger.info("Installing DCS BIOS...")
-                    dcs_bios_install(path_dcs)
+                    if dcs_bios_install(self.prefs.path_dcs) is None:
+                        PyGUI.Popup(f"DCS-BIOS failed to install.", title="Error")
                 except (FileExistsError, FileNotFoundError, requests.HTTPError) as e:
                     self.logger.error("DCS-BIOS failed to install", exc_info=True)
                     PyGUI.Popup(f"DCS-BIOS failed to install:\n{e}", title="Error")
                 self.update_gui_for_dcs_path()
     
             elif event == 'ux_path_dcs':
+                self.prefs_persist(self.values)
                 self.update_gui_for_dcs_path()
             
             elif event == 'ux_airframe_default' or event == 'ux_av_setup_default':
